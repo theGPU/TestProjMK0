c3037e0d1a8dc9064e427804a1d83a4206a83163
theCPU <logintitan@gmail.com>
2020-09-19 10:51:43 +0300

    update

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -11,12 +11,15 @@ namespace TestProjMK0
 
             foreach (DriveInfo disk in disks)
             {
+                Console.WriteLine("==================================");
+                Console.Write("Информация о диске - "); Console.WriteLine(disk.VolumeLabel);
                 Console.Write("Name - "); Console.WriteLine(disk.Name);
                 Console.Write("File system - "); Console.WriteLine(disk.DriveFormat);
                 Console.Write("Drive Type - "); Console.WriteLine(disk.DriveType);
                 Console.Write("Total size - "); Console.WriteLine(disk.TotalSize);
                 Console.Write("Total free space - "); Console.WriteLine(disk.TotalFreeSpace);
-
+                Console.WriteLine("==================================");
+                Console.WriteLine();
             }
         }
     }
