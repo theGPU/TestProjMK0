e4dec799a95a9448de56f58f4f193e77a8ca16ed
theCPU <logintitan@gmail.com>
2020-09-19 11:30:40 +0300

    Update F_MK0

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -5,10 +5,54 @@ namespace TestProjMK0
 {
     class Program
     {
+        static class BytesConvertet
+        {
+            public static float BytesToGB(long bytesCount)
+            {
+                return (float)bytesCount / 1073741824;
+            }
+            public static float BytesToMB(long bytesCount)
+            {
+                return (float)bytesCount / 1048576;
+            }
+            public static float BytesToKB(long bytesCount)
+            {
+                return (float)bytesCount / 1024;
+            }
+        }
+
+        delegate float ConvertToSize(long x);
+        static void PrintDiskSpace(string prefix, long bytes)
+        {
+            string postfix;
+            ConvertToSize operation;
+            if (bytes < 1048576)
+            {
+                postfix = " KB ";
+                operation = BytesConvertet.BytesToKB;
+            } else if (bytes < 1073741824)
+            {
+                postfix = " MB ";
+                operation = BytesConvertet.BytesToMB;
+            } else
+            {
+                postfix = " GB ";
+                operation = BytesConvertet.BytesToGB;
+            }
+
+            Console.Write(prefix + postfix + "- "); Console.WriteLine(operation(bytes));
+        }
+
+        static void PrintArray(string prefix, string[] array)
+        {
+            Console.WriteLine("=================="+ prefix +"================");
+            foreach (string str in array)
+                Console.WriteLine(str);
+        }
         static void Main(string[] args)
         {
             DriveInfo[] disks = DriveInfo.GetDrives();
-
+            
             foreach (DriveInfo disk in disks)
             {
                 Console.WriteLine("==================================");
@@ -16,8 +60,14 @@ namespace TestProjMK0
                 Console.Write("Name - "); Console.WriteLine(disk.Name);
                 Console.Write("File system - "); Console.WriteLine(disk.DriveFormat);
                 Console.Write("Drive Type - "); Console.WriteLine(disk.DriveType);
-                Console.Write("Total size - "); Console.WriteLine(disk.TotalSize);
-                Console.Write("Total free space - "); Console.WriteLine(disk.TotalFreeSpace);
+
+                Console.WriteLine("==================================");
+
+                PrintDiskSpace("Total size", disk.TotalSize);
+                PrintDiskSpace("Total free space", disk.TotalFreeSpace);
+
+                PrintArray("Директории", Directory.GetDirectories(disk.Name));
+                PrintArray("Файлы", Directory.GetFiles(disk.Name));
                 Console.WriteLine("==================================");
                 Console.WriteLine();
             }
